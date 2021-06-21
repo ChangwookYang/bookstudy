760c670e163f5a5695f52914daea0c10a363942b
양창욱(Changwook Yang)/Biz플랫폼개발팀/SKP <skp.1004683@sk.com>
2021-06-22 02:28:53 +0900

    Testcode

--- src/main/java/com/bookstudy/web/dto/HelloResponseDto.java
+++ src/main/java/com/bookstudy/web/dto/HelloResponseDto.java
@@ -1,4 +1,11 @@
 package com.bookstudy.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor // final 필드가 포함된 생성자를 생성해 준다.
 public class HelloResponseDto {
+    private final String name;
+    private final int amount;
 }
